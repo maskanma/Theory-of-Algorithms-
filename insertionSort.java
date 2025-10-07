3808af59ed17eeae9c5dbfb05b6ae6f8a08e0862
maskanma <9118729@stud.op.edu.ua>
2025-10-07 15:36:22 +0300

    Update insertionSort.java

--- insertionSort.java
+++ insertionSort.java
@@ -1,6 +1,6 @@
 public class InsertionSort {
     public static void main(String[] args) {
-        int[] arr = {69, 52, 97, 27, 10, 88, 29, 1, 24};
+        int[] arr = {53, 100, 44, 74, 53, 38, 82, 65, 28};
 
         System.out.println("Before sort:");
         printArray(arr);
