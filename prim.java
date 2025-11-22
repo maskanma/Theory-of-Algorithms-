4b5fa94b37ca98cead3367acb75f79279a9c782c
maskanma <9118729@stud.op.edu.ua>
2025-11-22 15:49:10 +0200

    Update prim.java

--- prim.java
+++ prim.java
@@ -4,21 +4,20 @@ public class Prim {
     private static final int INF = Integer.MAX_VALUE / 2;
 
     private static final int[][] G = {
-            //1      2      3      4      5      6      7      8
-            {INF,   INF,   2,     4,     INF,   8,     INF,   INF}, // 1
-            {INF,   INF,   7,     6,     INF,   3,     6,     INF}, // 2
-            {2,     7,     INF,   INF,   INF,   INF,   INF,   4  }, // 3
-            {4,     6,     INF,   INF,   1,     INF,   INF,   INF}, // 4
-            {INF,   INF,   INF,   1,     INF,   INF,   4,     INF}, // 5
-            {8,     3,     INF,   INF,   INF,   INF,   INF,   3  }, // 6
-            {INF,   6,     INF,   INF,   4,     INF,   INF,   5  }, // 7
-            {INF,   INF,   4,     INF,   INF,   3,     5,     INF}  // 8
+            {INF, 2,   5,   1,   INF, INF, 7,   INF},
+            {2,   INF, INF, 3,   6,   INF, INF, INF},
+            {5,   INF, INF, INF, 2,   4,   4,   INF},
+            {1,   3,   INF, INF, 5,   4,   9,   INF},
+            {INF, 6,   2,   5,   INF, 3,   INF, INF},
+            {INF, INF, 4,   4,   3,   INF, INF, 1  },
+            {7,   INF, 4,   9,   INF, INF, INF, 6  },
+            {INF, INF, INF, INF, INF, 1,   6,   INF}
     };
 
     public static void main(String[] args) {
 
         boolean[] selected = new boolean[V];
-        selected[0] = true; // start from vertex 1 (index 0)
+        selected[0] = true; // start from vertex 1
 
         int edgesUsed = 0;
         int totalWeight = 0;
@@ -29,6 +28,7 @@ public class Prim {
             int min = INF;
             int x = -1, y = -1;
 
+            // find smallest edge from selected -> not selected
             for (int i = 0; i < V; i++) {
                 if (selected[i]) {
                     for (int j = 0; j < V; j++) {
